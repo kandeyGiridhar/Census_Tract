8572843773001bdc2aa48981880b08ccf973c8de
kandeyGiridhar <40814477+kandeyGiridhar@users.noreply.github.com>
2019-09-11 19:55:57 +0530

    Update Census_Tract.R

--- Census_Tract.R
+++ Census_Tract.R
@@ -1,71 +1,80 @@
-library(httr)
-library(tidyverse)
-library(devtools)
-library(RCurl)
-library(urltools)
-library(DBI)
-library(odbc)
-library(RODBC)
+census <- function(tablename,query,DBname){
 
-#Establish database connection 
-connection <- DBI::dbConnect(odbc::odbc(), 
-                             Driver = "SQL Server", 
-                             Server = "172.28.30.25", 
-                             Database = "lara_data",
-                             UID = "prodigydba", 
-                             PWD = "3FsRxlD9ermiU1A!", 
-                             Port = 1433)
+  if(!require(pacman))utils::install.packages("pacman")
+  pacman::p_load("httr",
+                 "devtools",
+                 "RCurl",
+                 "urltools",
+                 "DBI",
+                 "odbc",
+                 "RODBC",
+                 "roxygen2",
+                 "usethis",
+                 "utils")
+  library(httr)
+  library(devtools)
+  library(RCurl)
+  library(urltools)
+  library(DBI)
+  library(odbc)
+  library(RODBC)
 
-#Query to fetch the data from SQL  
-Census_Tract<- DBI::dbGetQuery(connection,"select top 10 * from [test_geocoding]")
+  #Establish a database connection to retrieve input data
+  connection <- DBI::dbConnect(odbc::odbc(),
+                               Driver = "SQL Server",
+                               Server = "172.28.30.25",
+                               Database = paste(DBname),
+                               UID = Sys.getenv("UID"),
+                               PWD = Sys.getenv("PWD"),
+                               Port = 1433)
 
-#store the data in a temp .csv file
-input <- tempfile(fileext = ".csv")
-write.csv(Census_Tract, input, row.names = FALSE)
+  #SQL to specify which table to be used
+  Census_Tract<- DBI::dbGetQuery(connection,paste(query))
 
-#Function to conver the input address to desiered output
-apiurl <- "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
-list_output <- POST(apiurl, body= list(addressFile = upload_file(input), 
-                                       benchmark = "Public_AR_Current",
-                                       vintage = "Current_Current"
-), 
-encode="multipart"
-)
+  names(Census_Tract) <- NULL
+
+  #Create and store the data retrieved from the SQL (input data)
+  input <- tempfile(fileext = ".csv")
+  utils::write.csv(Census_Tract, input, row.names = FALSE)
+
+  #Function to implement the conversion of input address to the desired output
+  apiurl <- "https://geocoding.geo.census.gov/geocoder/geographies/addressbatch"
+  list_output <- httr::POST(apiurl, body= list(addressFile = httr::upload_file(input),
+                                               benchmark = "Public_AR_Current",
+                                               vintage = "Current_Current"
+  ),
+  encode="multipart"
+  )
+
+  #Cat function is used to print the output in user desired manner
+  cat(httr::content(list_output, "text", encoding = "UTF-8"), "\n")
+
+  #Store the encoded form of output in a file
+  cat(httr::content(list_output, "text", encoding = "UTF-8"), file="output.csv")
+
+  #convert the list format to a .csv format with column names
+  df_output<-utils::read.csv(file ="output.csv",header = FALSE,
+                             col.names = c("record_id_number"
+                                           ,"input_address"
+                                           ,"tiger_address_range_match_indicator"
+                                           ,"tiger_match_type"
+                                           ,"tiger_output_address"
+                                           ,"longitude_latitude"
+                                           ,"tigerline_id"
+                                           ,"tigerline_id_side"
+                                           ,"state_code"
+                                           ,"county_code"
+                                           ,"tract_code"
+                                           ,"block_code"))
+
+  #Store the output in a temp file
+  output <- tempfile(fileext = ".csv")
+  utils::write.csv(df_output, output, row.names = FALSE)
 
 
-
-content(list_output)
-
-
-cat(content(list_output, "text", encoding = "UTF-8"), "\n")
-
-#Storing the Encoded data in a file  
-cat(content(list_output, "text", encoding = "UTF-8"), file="output.csv")
-
-#Converting the output to a data frame
-df_output<-read.csv(file ="output.csv",header = FALSE,
-                    col.names = c("record_id_number"
-                                  ,"input_address"
-                                  ,"tiger_address_range_match_indicator" 
-                                  ,"tiger_match_type"
-                                  ,"tiger_output_address"
-                                  ,"longitude_latitude"
-                                  ,"tigerline_id"
-                                  ,"tigerline_id_side"
-                                  ,"state_code"
-                                  ,"county_code"
-                                  ,"tract_code"
-                                  ,"block_code"))
-
-#Removing the column headers from the output 
-df_output=subset(df_output,df_output$input_address!="street_address, city, state, zip")
-
-#store the output in a temp file 
-output <- tempfile(fileext = ".csv")
-write.csv(df_output, output, row.names = FALSE)
-
-#Storing the formated output in the database
-dbWriteTable(conn = connection, 
-             name = "Census_Tract", 
-             value = df_output,
-             overwrite = T)
+  #Establish the connection between SQL server and load the output data to tables
+  DBI::dbWriteTable(conn = connection,
+                    name = paste(tablename),
+                    value = df_output,
+                    overwrite = T)
+}
